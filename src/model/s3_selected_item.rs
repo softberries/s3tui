df967e25f3bfd9a812dcb1b2aed2210c8662fa81
Kris <krzysztof.grajek@googlemail.com>
2024-05-26 09:06:37 +0200

    ISSUE-4 simplified s3_selected_item.rs

--- src/model/s3_selected_item.rs
+++ src/model/s3_selected_item.rs
@@ -18,55 +18,6 @@ pub struct S3SelectedItem {
 }
 
 impl S3SelectedItem {
-    // pub fn new(name: String, bucket: Option<String>, path: Option<String>, is_directory: bool, is_bucket: bool, destination_dir: String, s3_creds: FileCredential, children: Option<Vec<S3SelectedItem>>) -> S3SelectedItem {
-    //     S3SelectedItem {
-    //         bucket,
-    //         name,
-    //         path,
-    //         is_directory,
-    //         is_bucket,
-    //         destination_dir,
-    //         transferred: false,
-    //         s3_creds,
-    //         progress: 0f64,
-    //         children,
-    //         error: None,
-    //     }
-    // }
-
-    /*
-    let selected_item = S3SelectedItem::new(
-                sr.name,
-                sr.bucket,
-                Some(sr.path),
-                sr.is_directory,
-                sr.is_bucket,
-                self.props.current_local_path.clone(),
-                creds,
-                None,
-            );
-   let selected_item = S3SelectedItem::new(
-                sr.name,
-                sr.bucket,
-                Some(sr.path),
-                sr.is_directory,
-                sr.is_bucket,
-                self.props.current_local_path.clone(),
-                creds,
-                None,
-            )
-    let selected_item = S3SelectedItem::new(
-                sr.name,
-                sr.bucket,
-                Some(sr.path),
-                sr.is_directory,
-                sr.is_bucket,
-                self.props.current_local_path.clone(),
-                creds,
-                None,
-            );
-     */
-
     pub fn from_s3_data_item(item: S3DataItem, creds: FileCredential, destination_dir: String) -> S3SelectedItem {
         S3SelectedItem {
             bucket: item.bucket,
@@ -127,18 +78,82 @@ mod tests {
             s3_creds: Default::default(),
             progress: 0f64,
             children: None,
-            error: None
+            error: None,
         };
-        let res = S3SelectedItem::new(
-            "file1.txt".into(),
-            Some("test-bucket".into()),
-            Some("path/to/file1.txt".into()),
-            false,
-            false,
-            "".into(),
-            Default::default(),
-            None
+        let s3_data_item = S3DataItem {
+            bucket: Some("test-bucket".into()),
+            name: "file1.txt".into(),
+            size: "100kB".into(),
+            file_type: "txt".into(),
+            path: "path/to/file1.txt".into(),
+            is_directory: false,
+            is_bucket: false,
+            region: Some("eu-west-1".into()),
+        };
+        let creds = FileCredential {
+            name: "personal".into(),
+            access_key: "abc".into(),
+            secret_key: "abc".into(),
+            default_region: "abc".into(),
+            selected: true,
+        };
+        let destination_dir = "/".into();
+        let res = S3SelectedItem::from_s3_data_item(
+            s3_data_item, creds, destination_dir,
         );
         assert_eq!(item, res);
+    }
+
+    #[test]
+    fn create_s3_selected_item_with_children_correctly() {
+        let child = S3SelectedItem {
+            bucket: Some("test-bucket".into()),
+            name: "file1.txt".into(),
+            path: Some("path/to/file1.txt".into()),
+            is_directory: false,
+            is_bucket: false,
+            destination_dir: "".to_string(),
+            transferred: false,
+            s3_creds: Default::default(),
+            progress: 0f64,
+            children: None,
+            error: None,
+        };
+        let item = S3SelectedItem {
+            bucket: Some("test-bucket".into()),
+            name: "file1.txt".into(),
+            path: Some("path/to/file1.txt".into()),
+            is_directory: false,
+            is_bucket: false,
+            destination_dir: "".to_string(),
+            transferred: false,
+            s3_creds: Default::default(),
+            progress: 0f64,
+            children: Some(vec![child.clone()]),
+            error: None,
+        };
+        let s3_data_item = S3DataItem {
+            bucket: Some("test-bucket".into()),
+            name: "file1.txt".into(),
+            size: "100kB".into(),
+            file_type: "txt".into(),
+            path: "path/to/file1.txt".into(),
+            is_directory: false,
+            is_bucket: false,
+            region: Some("eu-west-1".into()),
+        };
+        let creds = FileCredential {
+            name: "personal".into(),
+            access_key: "abc".into(),
+            secret_key: "abc".into(),
+            default_region: "abc".into(),
+            selected: true,
+        };
+        let destination_dir = "/".into();
+        let res = S3SelectedItem::from_s3_data_item_with_children(
+            s3_data_item, creds, destination_dir, vec![child]
+        );
+        assert_eq!(item, res);
+        assert_eq!(item.children.unwrap(), res.children.unwrap());
     }
 }
